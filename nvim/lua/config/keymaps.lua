e52b9b5b9de630addad8eb3afd80fa4d8e2aa8a6
paulo.sebastiao <paulo.sebastiao@bookbeat.com>
2026-09-21 18:18:37 +0200

    feat(nvim): keymaps: asked GTP to organize them more logically and do which-keys groups

--- nvim/lua/config/keymaps.lua
+++ nvim/lua/config/keymaps.lua
@@ -1,62 +1,79 @@
-vim.keymap.set("s", "n", "nzz") -- center screen on hits when navigating search results
+local map = vim.keymap.set
 
-vim.keymap.set("n", "<C-left>", "<C-W><left>", { desc = "Move cursor to windown on left"})
-vim.keymap.set("n", "<C-right>", "<C-W><right>", { desc = "Move cursor to windown on right"})
-vim.keymap.set("n", "<C-up>", "<C-W><up>", { desc = "Move cursor to windown above"})
-vim.keymap.set("n", "<C-down>", "<C-W><down>", { desc = "Move cursor to windown under"})
-
-vim.keymap.set("n", "<C-h>", "<C-W><left>", { desc = "Move cursor to windown on left"})
-vim.keymap.set("n", "<C-l>", "<C-W><right>", { desc = "Move cursor to windown on right"})
-vim.keymap.set("n", "<C-k>", "<C-W><up>", { desc = "Move cursor to windown above"})
-vim.keymap.set("n", "<C-j>", "<C-W><down>", { desc = "Move cursor to windown under"})
-
-vim.keymap.set("i", "jk", "<Esc>")
-
-
---- Diagnostics with mini.extra
----
---- ChatGPT suggested bindings - TODO Tweak
-local extra = require("mini.extra")
-
-
--- Diagnostics (prefix: [D]iagnostics)
-vim.keymap.set("n", "<leader>db", function() extra.pickers.diagnostic({ scope = "current" }) end, { desc = "Buffer diagnostics" })
-vim.keymap.set("n", "<leader>dw", function() extra.pickers.diagnostic({ scope = "all" }) end, { desc = "Workspace diagnostics" })
-
-vim.keymap.set("n", "K", vim.lsp.buf.hover, { desc = "Hover documentation", })
-vim.keymap.set("n", "<leader>dd", vim.diagnostic.open_float, { desc = "Diagnostic details", })
-
--- LSP navigation (prefix: [C]ode)
-vim.keymap.set("n", "<leader>cfr", function() extra.pickers.lsp({ scope = "references" }) end, { desc = "References" })
-vim.keymap.set("n", "<leader>cds", function() extra.pickers.lsp({ scope = "document_symbol" }) end, { desc = "Document symbols" })
-vim.keymap.set("n", "<leader>cws", function() extra.pickers.lsp({ scope = "workspace_symbol" }) end, { desc = "Workspace symbols" })
-vim.keymap.set({ "n", "v" }, "<leader>ca", vim.lsp.buf.code_action, { desc = "Code actions", })
-
-vim.keymap.set("n", "<leader>rn", vim.lsp.buf.rename, { desc = "Rename symbol", })
-
--- Other useful lists (Buffers = [B]uffers)
+local fff = require("fff")
 local MiniPick = require("mini.pick")
-vim.keymap.set("n", "<leader>bs", function() MiniPick.builtin.buffers() end, { desc = "Switch between buffers" })
-vim.keymap.set("n", "<leader>bh", function() MiniPick.builtin.help() end, { desc = "Help" })
-
--- Navigation ([F]iles)
-vim.keymap.set("n", "<leader>fe", function()
-  require("nvim-tree.api").tree.open({
-    path = vim.fn.getcwd(),
-  })
-end, { desc = "Open file tree at current directory" })
-
-vim.keymap.set("n", "<leader>fc", function()
-	MiniPick.builtin.files({
-   		 cwd = vim.fn.stdpath("config"),
-  	})
-end
-, { desc = "[c]onfiguration files"})
-
 local MiniFiles = require("mini.files")
+local MiniExtra = require("mini.extra")
+local NvimTree = require("nvim-tree.api")
 
-local minifiles_toggle = function(...)
-    if MiniFiles.close() == nil then MiniFiles.open(...) end
-  end
+-- General
+map("i", "jk", "<Esc>", { desc = "Exit insert mode" })
+map("n", "n", "nzz", { desc = "Next search result centered" })
+map("n", "N", "Nzz", { desc = "Previous search result centered" })
 
-vim.keymap.set("n", "<leader>fp", function() minifiles_toggle() end , { desc = "Open Picker" })
+-- Window navigation
+map("n", "<C-h>", "<C-w>h", { desc = "Window left" })
+map("n", "<C-j>", "<C-w>j", { desc = "Window down" })
+map("n", "<C-k>", "<C-w>k", { desc = "Window up" })
+map("n", "<C-l>", "<C-w>l", { desc = "Window right" })
+map("n", "<C-left>", "<C-w>h", { desc = "Window left" })
+map("n", "<C-down>", "<C-w>j", { desc = "Window down" })
+map("n", "<C-up>", "<C-w>k", { desc = "Window up" })
+map("n", "<C-right>", "<C-w>l", { desc = "Window right" })
+
+-- LSP navigation
+map("n", "K", vim.lsp.buf.hover, { desc = "Hover documentation" })
+map("n", "gd", vim.lsp.buf.definition, { desc = "Go to definition" })
+map("n", "gD", vim.lsp.buf.declaration, { desc = "Go to declaration" })
+
+-- Diagnostics
+map("n", "[d", function() vim.diagnostic.jump({ count = -1 }) end, { desc = "Previous diagnostic" })
+map("n", "]d", function() vim.diagnostic.jump({ count = 1 }) end, { desc = "Next diagnostic" })
+map("n", "<leader>db", function() MiniExtra.pickers.diagnostic({ scope = "current" }) end, { desc = "Buffer diagnostics" })
+map("n", "<leader>dw", function() MiniExtra.pickers.diagnostic({ scope = "all" }) end, { desc = "Workspace diagnostics" })
+map("n", "<leader>dd", vim.diagnostic.open_float, { desc = "Diagnostic details" })
+
+-- Code / LSP
+map({ "n", "v" }, "<leader>ca", vim.lsp.buf.code_action, { desc = "Code action" })
+map("n", "<leader>cr", vim.lsp.buf.rename, { desc = "Rename symbol" })
+map("n", "<leader>cR", function() MiniExtra.pickers.lsp({ scope = "references" }) end, { desc = "References" })
+map("n", "<leader>cs", function() MiniExtra.pickers.lsp({ scope = "document_symbol" }) end, { desc = "Document symbols" })
+map("n", "<leader>cS", function() MiniExtra.pickers.lsp({ scope = "workspace_symbol" }) end, { desc = "Workspace symbols" })
+map("n", "<leader>ci", function() MiniExtra.pickers.lsp({ scope = "implementation" }) end, { desc = "Implementations" })
+
+-- Buffers
+map("n", "<leader>bb", function() MiniPick.builtin.buffers() end, { desc = "Switch buffer" })
+map("n", "<leader>bd", "<cmd>bdelete<cr>", { desc = "Delete buffer" })
+map("n", "<leader>bn", "<cmd>bnext<cr>", { desc = "Next buffer" })
+map("n", "<leader>bp", "<cmd>bprevious<cr>", { desc = "Previous buffer" })
+
+-- Files / Find
+
+local function nvimtree_toggle()
+  NvimTree.tree.toggle({ path = vim.fn.getcwd(), focus = true })
+end
+map("n", "<leader>fe", nvimtree_toggle, { desc = "Toggle file explorer" })
+map("n", "<leader>fm", function() MiniFiles.open() end, { desc = "Toggle mini.files" })
+map("n", "<leader>ff", function() fff.find_files() end, { desc = "Find files" })
+map("n", "<leader>fg", function() fff.live_grep() end, { desc = "Live grep" })
+map("n", "<leader>fz", function() fff.live_grep({ grep = { modes = { "fuzzy", "plain" } } }) end, { desc = "Fuzzy grep" })
+map({ "n", "x" }, "<leader>fw", function() fff.live_grep_under_cursor() end, { desc = "Grep word / selection" })
+
+-- Help
+map("n", "<leader>h", function() MiniPick.builtin.help() end, { desc = "Help" })
+
+-- Which-key
+local ok, wk = pcall(require, "which-key")
+
+if ok then
+  wk.setup({ delay = 300 })
+
+  wk.add({
+    { "<leader>b", group = "Buffers" },
+    { "<leader>c", group = "Code" },
+    { "<leader>d", group = "Diagnostics" },
+    { "<leader>f", group = "Files / Find" },
+    { "<leader>g", group = "Git" },
+    { "<leader>t", group = "Tests" },
+  })
+end
