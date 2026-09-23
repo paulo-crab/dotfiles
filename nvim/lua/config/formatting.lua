47a13c352483f80d098f9626ad2032186df21484
paulo.sebastiao <paulo.sebastiao@bookbeat.com>
2026-09-23 12:40:18 +0200

    feat(nvim): Added formatter 'conform'. Configured csharpier as default for cs files.

--- nvim/lua/config/formatting.lua
+++ nvim/lua/config/formatting.lua
@@ -1,3 +1,23 @@
 require("mini.pairs").setup()
 require("mini.surround").setup()
 require("mini.ai").setup()
+
+require("conform").setup({
+	formatters_by_ft = {
+		lua = { "stylua" },
+		-- Conform will run multiple formatters sequentially
+		cs = { "csharpier" },
+		python = { "isort" },
+		-- You can customize some of the format options for the filetype (:help conform.format)
+		rust = { "rustfmt", lsp_format = "fallback" },
+		-- Conform will run the first available formatter
+		javascript = { "prettierd", "prettier", stop_after_first = true },
+	},
+})
+
+vim.api.nvim_create_autocmd("BufWritePre", {
+	pattern = "*",
+	callback = function(args)
+		require("conform").format({ bufnr = args.buf })
+	end,
+})
